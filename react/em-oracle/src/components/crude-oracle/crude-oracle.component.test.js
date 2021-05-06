b3642a606caaa7b35d1d6c9fa5490bf1ec8dea76
Marco Dierenfeldt <marco.dierenfeldt@gmail.com>
2021-05-06 11:41:17 +0200

    add some tests

--- react/em-oracle/src/components/crude-oracle/crude-oracle.component.test.js
+++ react/em-oracle/src/components/crude-oracle/crude-oracle.component.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import CrudeOracleComponent from './crude-oracle.component';
 
-test('renders learn react link', () => {
+test('renders headline', () => {
   render(<CrudeOracleComponent />);
   const hewadlineElement = screen.getByTestId("matchHeadline");
   expect(hewadlineElement).toBeInTheDocument();
+});
+
+test('renders game result', () => {
+  render(<CrudeOracleComponent />);
+  const resultElement = screen.getByTestId("gameResult");
+  expect(resultElement).toBeInTheDocument();
+});
+
+test('renders winner', () => {
+  render(<CrudeOracleComponent />);
+  const winnerProclamation = screen.getByTestId("winnerProclamation");
+  expect(winnerProclamation).toBeInTheDocument();
 });
